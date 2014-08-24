7810a939b948a02a328104afb88034d8c4c63ed9
Badger <h.wood.l@163.com>
2014-08-24 20:50:22 +0800

    First File

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+#coding=utf-8
 __author__ = 'badger'
+numberlist = []
+NumCount = 0
+startNum = 2L
+endNum = 10000L
+message = str(startNum) + '到' + str(endNum) + '内素数：\n'
+print(message)
+
+for i in xrange(startNum, endNum - 1L, 1L):
+    count = 0L
+    for n in xrange(2L, i, 1L):
+        if(i % n == 0):
+            count += 1L
+            break
+    if(count == 0):
+        NumCount += 1L
+        message = '第' + str(NumCount) + '个:' + str(i)
+        print(message)
